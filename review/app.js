f9d7c24f5f5fa14d08ebb558b1eafac6dc9395b8
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2026-09-17 10:36:09 +0900

    Publish review page artifacts

--- review/app.js
+++ review/app.js
@@ -2218,7 +2218,7 @@ function normalizeCorrectionSourceText(value) {
 function hiraganaToKatakana(value) {
   return String(value || "").replace(/[\uff66-\uff9f]+/gu, (text) => text.normalize("NFKC")).replace(/[ぁ-ゖ]/gu, (char) =>
     String.fromCharCode(char.charCodeAt(0) + 0x60),
-  );
+  ).replace(/[ァ-ヺ\u3099\u309a]+/gu, (text) => text.normalize("NFC"));
 }
 
 function validateRenderedYomiReading(surface, reading) {
